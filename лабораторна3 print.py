941581c60e0cd3aa3c80e188e7b2a1a687ace194
Tretyacova <90208072+Tretyacova@users.noreply.github.com>
2021-11-23 13:57:18 +0200

    lab3

---
+++ лабораторна3 print.py
@@ -0,0 +1,26 @@
+print("Гpyna 4") 
+text_file = open("D:\students4.txt", "wt") 
+text_file.write("Дашкевич Данило, 2 балів\n") 
+text_file.write("Євдокименко Олександр, 5 балів\n") 
+text_file.write("Карбівничный Володимир, 7 балів\n") 
+text_file.write("Сухина Денис, 8 балів\n") 
+text_file.close() 
+ 
+print("Гpyna 5") 
+text_file = open("D:\students5.txt", "wt") 
+text_file.write("Муляр Вікторія, 5 балів\n") 
+text_file.write("Третьякова Вікторія, 9 балів\n") 
+text_file.write("Омелянчук Роман, 9 балів\n") 
+text_file.write("Лукін Володимир, 10 балів\n") 
+text_file.close() 
+ 
+print("Гpyna 6") 
+text_file = open("D:\students6.txt", "wt") 
+text_file.write("Петрушка Кирило, 3 балів\n") 
+text_file.write("Мазур Максим, 7 балів\n") 
+text_file.write("Піць Андрій, 10 балів\n") 
+text_file.write("Микуланинець Володимир, 8 балів\n") 
+text_file.close()
+
+
+input()
